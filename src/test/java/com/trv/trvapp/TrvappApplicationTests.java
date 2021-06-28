a69f9712b37f71289814115f13b6a5de3d939e66
IW.MOON <=>
2021-06-28 21:58:14 +0900

    init

---
+++ src/test/java/com/trv/trvapp/TrvappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trv.trvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrvappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
