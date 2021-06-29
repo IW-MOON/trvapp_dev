3d98d60872d2310b4cc91f253604c0a4703aaa1e
IW.MOON <=>
2021-06-29 23:02:23 +0900

    init

--- src/test/java/com/trv/trvapp/TrvappApplicationTests.java
+++ src/test/java/com/trv/trvapp/TrvappApplicationTests.java
@@ -1,9 +1,11 @@
 package com.trv.trvapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 class TrvappApplicationTests {
 
 	@Test
