33a56bae0c0d597fdadd366e2bd80bd266972aa4
IW.MOON <=>
2021-06-29 22:54:00 +0900

    init

--- src/main/java/com/trv/trvapp/TrvappApplication.java
+++ src/main/java/com/trv/trvapp/TrvappApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TrvappApplication {
 
+	static {
+
+		//System.out.println(System.getProperty("docker-compose.yml"));
+		//System.setProperty("spring.config.location", "file:/application.yml");
+		//System.setProperty("spring.config.location", "classpath:/application.yml");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrvappApplication.class, args);
 	}
---
+++ src/main/java/com/trv/trvapp/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.trv.trvapp.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@PropertySource( value = "file:./application.yml", ignoreResourceNotFound = true)
+public class HelloController {
+
+    @Value("${logging-module.version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+
+    }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+
+}
