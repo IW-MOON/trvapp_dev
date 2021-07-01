79ac52e7de577d32f8dbc7ea871d45e17d65e60b
IW.MOON <=>
2021-07-01 22:09:39 +0900

    init

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -3,6 +3,7 @@
 # !/bin/bash
 #
 
+cp /home/ubuntu/app/trvapp/application.yml .
 source /home/ubuntu/app/trvapp/deploy_env.sh
 
 CURRENT_PORT=$(cat /etc/nginx/conf.d/service_url.inc | grep -Po '[0-9]+' | tail -1)
@@ -11,8 +12,10 @@ TARGET_PORT=0
 echo "> Current port of running WAS is ${CURRENT_PORT}."
 echo ${DEPLOYMENT_ACTIVE}
 if [ ${DEPLOYMENT_ACTIVE} == "dev" ]; then
+  cp /home/ubuntu/app/trvapp/application-dev.yml .
   docker-compose -f /home/ubuntu/app/trvapp/trvapp/docker-compose.yml up -d
 elif [ ${DEPLOYMENT_ACTIVE} == "prod" ]; then
+  cp /home/ubuntu/app/trvapp/trvapp/build/libs/* /home/ubuntu/app/trvapp/trvapp_prod/
   docker-compose -f /home/ubuntu/app/trvapp/trvapp_prod/docker-compose.yml up -d
 else
   echo "> DEPLOYMENT_ACTIVE is not correct "
